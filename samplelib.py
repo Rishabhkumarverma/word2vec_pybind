34dd9a6a65aa116e35c8276b4adfacc5b4de11a4
Rishabhkumarverma <im.er.rishabh@gmail.com>
2020-03-15 18:33:22 +0530

    code refactor

--- samplelib.py
+++
@@ -1,6 +0,0 @@
-from build import PyProfiler
-path_model="/home/risverm/workspace/project/Word2vec/data/GoogleNews-vectors-negative300.bin"
-path_doc= "/home/risverm/workspace/project/word2vec-master/examples/doc2vec/texts/bbc_brazil_meat.txt"
-obj=PyProfiler.CPyProfiler()
-print(obj.myadd(1,2))
-print(obj.doc2vector(path_doc,path_model))
