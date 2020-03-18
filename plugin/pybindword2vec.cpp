63ecd31db722d3e9d239cf6df69ec0ec298b7872
Rishabhkumarverma <im.er.rishabh@gmail.com>
2020-03-18 19:56:25 +0530

    changed the inteface

--- plugin/pybindword2vec.cpp
+++ plugin/pybindword2vec.cpp
@@ -1,6 +1,6 @@
 #include "pybindword2vec.hpp"
 #include <pybind11/pybind11.h>
-
+#include <pybind11/stl.h>
 PYBIND11_MODULE(word2vec, m)
 {
   namespace py = pybind11;
@@ -9,7 +9,11 @@ PYBIND11_MODULE(word2vec, m)
       .def(py::init<>())
         // The destroy_plugin function does not override the base class, so we must bind it explicitly.
       .def("myadd", &CWord2Vec::myadd)
-      .def("doc2vector", &CWord2Vec::run);
+      .def("loadWord2Vec", &CWord2Vec::loadWord2Vec)
+      .def("loadDoc2VecModel", &CWord2Vec::loadDoc2VecModel)
+      .def("saveDoc2VecModel", &CWord2Vec::saveDoc2VecModel)
+      .def("doc2vector", &CWord2Vec::convertDoc2Vec)
+      .def("doc2vectorSimilarity", &CWord2Vec::Doc2VecSimilarity);
       //.def(py::init<PyProfiler::reportLayerTime_t>());
 }
 
--- plugin/pybindword2vec.hpp
+++ plugin/pybindword2vec.hpp
@@ -10,13 +10,73 @@
 
 class CWord2Vec
 {
-public:
+private:
+std::unique_ptr<w2v::w2vModel_t> w2vModel;
+std::unique_ptr<w2v::d2vModel_t> d2vModel;
+// get nearest article IDs from the model
+std::vector<std::pair<std::size_t, float>> nearest;
 void readFile(const std::string &_fileName, std::string &_data);
-int run(const std::string modelname, const std::string filename );
+public:
+//CWord2Vec(const std::string &_fileName); 
+int loadWord2Vec(const std::string &_fileName);
+int loadDoc2VecModel(const std::string &_fileName);
+void saveDoc2VecModel(const std::string outfilename);
+int convertDoc2Vec(const std::string filename,int id);
+std::vector<std::pair<std::size_t, float>> Doc2VecSimilarity(const std::string filename, int max_nearest_vec);
 int myadd(int a , int b){return(a+b);}
     
 };
 
+/*CWord2Vec::CWord2Vec(const std::string &_fileName){
+   try {
+        w2vModel.reset(new w2v::w2vModel_t());
+        // load w2v model file
+        if (!w2vModel->load(_fileName)) {
+            throw std::runtime_error(w2vModel->errMsg());
+        }
+    } catch (const std::exception &_e) {
+        std::cerr << _e.what() << std::endl;
+        return 2;
+    } catch (...) {
+        std::cerr << "unknown error" << std::endl;
+        return 2;
+    }
+}*/
+int CWord2Vec::loadWord2Vec(const std::string &_fileName){
+   try {
+        this->w2vModel.reset(new w2v::w2vModel_t());
+        // load w2v model file
+        if (!w2vModel->load(_fileName)) {
+            throw std::runtime_error(w2vModel->errMsg());
+        }
+         this->d2vModel.reset(new w2v::d2vModel_t(this->w2vModel->vectorSize()));
+    } catch (const std::exception &_e) {
+        std::cerr << _e.what() << std::endl;
+        return 2;
+    } catch (...) {
+        std::cerr << "unknown error" << std::endl;
+        return 2;
+    }
+   return 0;
+}
+
+
+int CWord2Vec::loadDoc2VecModel(const std::string &_fileName){
+   try {
+        this->d2vModel.reset(new w2v::d2vModel_t(this->w2vModel->vectorSize()));
+        // load w2v model file
+        if (!d2vModel->load(_fileName)) {
+            throw std::runtime_error(w2vModel->errMsg());
+        }
+    } catch (const std::exception &_e) {
+        std::cerr << _e.what() << std::endl;
+        return 2;
+    } catch (...) {
+        std::cerr << "unknown error" << std::endl;
+        return 2;
+    }
+   return 0;
+}
 
 void CWord2Vec::readFile(const std::string &_fileName, std::string &_data) {
     std::ifstream ifs;
@@ -30,50 +90,24 @@ void CWord2Vec::readFile(const std::string &_fileName, std::string &_data) {
     ifs.close();
 }
 
-int CWord2Vec::run(const std::string modelname, const std::string filename)
-{
-
-// load wor2vec model
-    std::unique_ptr<w2v::w2vModel_t> w2vModel;
-    try {
-        w2vModel.reset(new w2v::w2vModel_t());
-        // load w2v model file
-        if (!w2vModel->load(filename)) {
-            throw std::runtime_error(w2vModel->errMsg());
+void CWord2Vec::saveDoc2VecModel(const std::string outfilename){
+   // finaly, save our doc2vec model
+        if (!this->d2vModel->save(outfilename)) {
+            std::cerr << "Can not save model: " << this->d2vModel->errMsg() << std::endl;
         }
-    } catch (const std::exception &_e) {
-        std::cerr << _e.what() << std::endl;
-        return 2;
-    } catch (...) {
-        std::cerr << "unknown error" << std::endl;
-        return 2;
-    }
+}
 
-    // create doc2vec model
-    w2v::d2vModel_t d2vModel(w2vModel->vectorSize());
-    try {
-        
-        /* load independent_orly_shooting.txt text
-         * We will try to find articles closest to this one.
-        */
+int CWord2Vec::convertDoc2Vec(const std::string filename, int id)
+{       
+       
+       try {
         std::string fileText ;
-        readFile(std::string(modelname), fileText);
+        this->readFile(std::string(filename), fileText);
         //  text to vector
-        w2v::doc2vec_t doc2vec(w2vModel, fileText);
-
-        // get nearest article IDs from the model
-        std::vector<std::pair<std::size_t, float>> nearest;
-        d2vModel.nearest(doc2vec, nearest, d2vModel.modelSize());
-
-        // output result set
-        for (auto const &i:nearest) {
-            std::cout << i.first << ": " << i.second << std::endl;
-        }
-
-        // finaly, save our doc2vec model
-        if (!d2vModel.save("model.d2v")) {
-            std::cerr << "Can not save model: " << d2vModel.errMsg() << std::endl;
-        }
+        w2v::doc2vec_t doc2vec(this->w2vModel, fileText);
+        // add vector with ID = 1 to the model
+        this->d2vModel->set(id, doc2vec);
+        
     } catch (const std::exception &_e) {
         std::cerr << _e.what() << std::endl;
         return 3;
@@ -81,6 +115,34 @@ int CWord2Vec::run(const std::string modelname, const std::string filename)
         std::cerr << "unknown error" << std::endl;
         return 3;
     }
+     
  return 0;
 }
+
+std::vector<std::pair<std::size_t, float>> CWord2Vec::Doc2VecSimilarity(const std::string filename, int max_nearest_vec)
+{       
+         this->nearest.clear();
+         //std::vector<std::pair<std::size_t, float>>
+     try {
+        std::string fileText ;
+        this->readFile(std::string(filename), fileText);
+        //  text to vector
+        w2v::doc2vec_t doc2vec(this->w2vModel, fileText);
+        this->d2vModel->nearest(doc2vec, this->nearest, max_nearest_vec);
+        } catch (const std::exception &_e) {
+        std::cerr << _e.what() << std::endl;
+    } catch (...) {
+        std::cerr << "unknown error" << std::endl;
+    }
+      // output result set
+        
+        for (auto const &i:nearest) {
+            std::cout << i.first << ": " << i.second << std::endl;
+            
+        }
+    return this->nearest;
+}
+
+
+
 #endif //_FULLY_CONNECTED_H
